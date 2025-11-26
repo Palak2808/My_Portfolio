221cc0992ce4e9664e24e733be95e7de3bd6ace1
palakg2g <palak1@gulp2go.com>
2025-11-26 17:28:19 +0530

    Initial commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,91 @@
+import { useEffect, useRef, useState } from "react";
+import OverLayMenu from "./OverLayMenu";
+import logo from "../assets/logo.png";
+import { FiMenu } from "react-icons/fi";
+
+export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false); //whether the overlay menu is open
+  const [visible, setVisible] = useState(true); //whether the navbar is visible
+  const [forceVisible, setforceVisible] = useState(false); //Forces navbar to remain visible when at the top
+
+  const lastScrollY = useRef(0); //store posoition for last scroll
+  const timerId = useRef(0);
+  useEffect(() => {
+    const homeSection = document.querySelector("#home");
+    const observer = new IntersectionObserver( //Create an observer to watch when #home is in the viewport.
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setforceVisible(true);
+          setVisible(true); //If near the top, navbar must stay visible and ignore auto-hide logic.
+        } else setforceVisible(false);
+      },
+      { threshold: 0.1 } //at least 10% of the home section is visible
+    );
+    if (homeSection) observer.observe(homeSection); //Start observing.
+    return () => {
+      if (homeSection) observer.unobserve(homeSection);
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (forceVisible) {
+        //If near top section → navbar always visible.
+        setVisible(true);
+        return;
+      }
+      const currentScrollY = window.scrollY;
+      if (currentScrollY > lastScrollY.current && currentScrollY > 100) {
+        //scrolling down
+        setVisible(false);
+      } else {
+        //scrolling up
+        setVisible(true);
+      }
+      if (timerId.current) clearTimeout(timerId.current);
+      timerId.current = setTimeout(() => {
+        //If user stops scrolling → hide navbar after 2 seconds.
+        setVisible(false);
+      }, 2000);
+      lastScrollY.current = currentScrollY; //Update last scroll position.
+    };
+    window.addEventListener("scroll", handleScroll, { passive: true });
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      if (timerId.current) clearTimeout(timerId.current);
+    };
+  }, [forceVisible]);
+
+  return (
+    <>
+      <nav
+        className={`fixed top-0 left-0 w-full flex items-center justify-between px-6 py-4 z-50 transition-transform duration-300 ${
+          visible ? "translate-y-0" : "-translate-y-full"
+        }`}
+      >
+        <div className="flex items-center space-x-2">
+          <img src={logo} alt="logo" className="w-8 h-8" />
+          <p className="font-bold text-2xl text-white sm:block">Palak</p>
+        </div>
+        <div className="block lg:absolute lg:left-1/2 lg:transform lg:translate-x-1/2">
+          <button
+            onClick={() => setMenuOpen(true)}
+            className="text-white text-3xl focus:outline-none"
+            aria-label="open Menu"
+          >
+            <FiMenu />
+          </button>
+        </div>
+        <div className="hidden lg:block">
+          <a
+            href="#Contact"
+            className="bg-gradient-to-r from-pink-500 to-blue-500 text-white py-2 px-5 rounded-full font-medium shadow-lg hover:opacity-90 transition-opacity duration-300"
+          >
+            Reach Out
+          </a>
+        </div>
+      </nav>
+      <OverLayMenu isOpen={menuOpen} onClose={() => setMenuOpen(false)} />
+    </>
+  );
+}
---
+++ src/components/ParticlesBackground.tsx
@@ -0,0 +1,89 @@
+import { useEffect, useRef } from "react";
+
+export default function ParticlesBackground() {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  useEffect(() => {
+    const canvas = canvasRef.current!; //access actual dom element, ! tells TypeScript that you know it won’t be null.
+    if (!canvas) return;
+    const ctx = canvas.getContext("2d")!;
+    if (!ctx) return;
+
+    let particles: Particle[] = [];
+    const particleCount = 50;
+    const colors = ["rgba(255,255,255,0.7)"];
+
+    class Particle {
+      x: number;
+      y: number;
+      radius: number;
+      color: string;
+      speedX: number;
+      speedY: number;
+
+      constructor() {
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height; //Random starting X/Y positions.
+        this.radius = Math.random() * 2 + 1; //Each particle gets a random size between 1 and 3px.
+        this.color = colors[Math.floor(Math.random() * colors.length)];
+        this.speedX = (Math.random() - 0.5) * 0.5; //Random speed from -0.25 to +0.25.
+        this.speedY = (Math.random() - 0.5) * 0.5;
+      }
+
+      draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2); //circle shaped
+        ctx.shadowBlur = 10;
+        ctx.shadowColor = this.color;
+        ctx.fillStyle = this.color;
+        ctx.fill();
+      }
+
+      update() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+        if (this.x < 0 || this.x > canvas.width) this.speedX = -this.speedX; //Bounce off the edges.
+        if (this.y < 0 || this.y > canvas.height) this.speedY = -this.speedY;
+
+        this.draw(); //Draws the updated particle.
+      }
+    }
+
+    function createParticles() {
+      particles = [];
+      for (let i = 0; i < particleCount; i++) {
+        particles.push(new Particle());
+      }
+    }
+
+    function handleResize() {
+      //Adjusts canvas size when window resizes.
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+      createParticles();
+    }
+
+    handleResize(); //Set initial canvas size.
+    window.addEventListener("resize", handleResize); //Listen for window resizing.
+
+    let animationId: number;
+    function animate() {
+      ctx.clearRect(0, 0, canvas.width, canvas.height); //Clears previous frame.
+      particles.forEach((particle) => particle.update());
+      animationId = requestAnimationFrame(animate);
+    }
+    animate();
+
+    return () => {
+      cancelAnimationFrame(animationId);
+      window.removeEventListener("resize", handleResize); //Stops animation and removes event listeners when component unmounts.
+    };
+  }, []);
+  return (
+    <canvas
+      ref={canvasRef}
+      className="fixed top-0 left-0 w-full h-full pointer-events-none"
+    >
+      ParticlesBackground
+    </canvas>
+  );
+}
